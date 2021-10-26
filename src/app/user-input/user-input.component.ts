945039974eef69cc383c5e03b6c7fe5c7457dde0
Johan <johanxgamble@gamil.com>
2021-10-26 13:05:12 +0200

    docs folder generated

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -3,7 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
-  styleUrls: ['./user-input.component.css']
+  styleUrls: ['./user-input.component.sass']
 })
 export class UserInputComponent {
   @Output() submittedInput = new EventEmitter<string>();
